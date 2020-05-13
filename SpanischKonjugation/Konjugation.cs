b0a03686e15737730cc72e829b0c40998381aa23
LeonKlemmer <64715493+LeonKlemmer@users.noreply.github.com>
2020-05-13 23:59:59 +0200

    Konjugation
    Version 1.0.0

---
+++ SpanischKonjugation/Konjugation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpanischKonjugation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
